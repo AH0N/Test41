1673290dec5698f77c906807488a1215455fd4d1
AH0N <573071538@qq.com>
2020-02-11 12:42:47 +0800

    Test commit

--- app/src/main/java/com/qy/test4/MainActivity.java
+++ app/src/main/java/com/qy/test4/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"test", Toast.LENGTH_SHORT).show();
     }
 
     public void c1(View view) {
